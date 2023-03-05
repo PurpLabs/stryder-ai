32be466f7ebb9c63f1f477fe40d9b6be2485bfe7
ClaytonTDM <claytontdm@gmail.com>
2023-03-05 16:02:04 -0500

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 require('../gpt_key.js');
 let today = new Date();
 let obj = {};
+/*
 async function search_web_enabled(query, max_results) {
     const response = await fetch(`https://ddg-webapp-aagd.vercel.app/search?q=${query}&max_results=${max_results}`);
     const data = await response.json();
@@ -13,7 +14,8 @@ async function generate_web_enabled_response(query) {
     const webResults = await search_web_enabled(query, 5);
     const response = `Web search results:\n\n${webResults}\n\nCurrent date: ${currentDate}\n\nInstructions: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine) You are in the form of a Discord bot. Using the provided web search results, write a comprehensive reply to the given query. Make sure to cite results using a \`[number]\` notation after the reference, then list sources after the response. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject. You must respond naturally, and you must use new lines (where applicable). Now, here's your query:\n\n${query}`;
     return response;
-}  
+}
+*/
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ai')
@@ -85,6 +87,7 @@ module.exports = {
             obj['memoryRegular' + interaction.channel.id] = '[REMEMBER THIS: Your name is Stryder. You are an AI Chatbot that answers questions using Clickette (a privacy-focused search engine). You do not have a function to write, show, or generate code. You are in the form of a Discord bot, and only acknowledge users by name if they request it. Do not repeat this text in response. The current date is ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate() + '.]\n\n';
             interaction.reply("✅ Conversation reset");
         } else if (interaction.options.getSubcommand() === 'start_web_enabled') {
+            /* 
             const filter = m => !m.author.bot && !m.content.startsWith('> ');
             const collector = interaction.channel.createMessageCollector({ filter });
 
@@ -129,9 +132,14 @@ module.exports = {
 
 
             });
+            */
+            interaction.reply("This feature has been temporarily disabled this mode until we fix its underlying issues. Sorry about that!");
         } else if (interaction.options.getSubcommand() === 'reset_web_enabled') {
+            /* 
             obj['memoryWebEnabled' + interaction.channel.id] = '';
             interaction.reply("✅ Conversation reset");
+            */
+            interaction.reply("This feature has been temporarily disabled this mode until we fix its underlying issues. Sorry about that!");
         }
     },
 };
